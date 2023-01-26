0bb90a67941361afcd8e40801cccd42e42e1e6ee
Mladen <mladenjovanovic95@hotmail.com>
2023-01-26 13:22:41 +0100

    repo init, folderstructure backend, models init, webconfig angelegt, controller init

---
+++ Backend/src/main/java/technikumbackendfrontendproject/Backend/controller/productController.java
@@ -0,0 +1,12 @@
+package technikumbackendfrontendproject.Backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class productController {
+
+    private final ProductRepository repo = new List
+    
+}
