f22258730fe72859c5eafe47794d0f6a935bf728
Mladen <mladenjovanovic95@hotmail.com>
2023-07-28 10:22:46 +0200

    Js für das Login Modal, Form verschoben, erste Versuche Daten per Post zu schicken

--- Frontend/components/loginModal/loginModal.js
+++ Frontend/components/loginModal/loginModal.js
@@ -1,1 +1,12 @@
 $("#loginModalContainer").load("/Frontend/components/loginModal/loginModal.html");
+
+$(document).on("submit", "#loginForm", function(event) {
+    event.preventDefault();
+    login();
+});
+
+function login() {
+    const username = document.getElementById("usernameLogin").value;
+    const password = document.getElementById("passwordLogin").value;
+    console.log(username, password);
+}
