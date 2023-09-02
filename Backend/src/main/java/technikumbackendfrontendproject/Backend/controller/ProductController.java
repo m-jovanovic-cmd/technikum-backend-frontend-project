4881665b6834b43424784985df1b06e49ac114dd
jesstify <jessica.fritsch@hotmail.com>
2023-09-02 14:05:07 +0200

    try to pass the id parameter with html

--- Backend/src/main/java/technikumbackendfrontendproject/Backend/controller/ProductController.java
+++ Backend/src/main/java/technikumbackendfrontendproject/Backend/controller/ProductController.java
@@ -3,6 +3,7 @@ package technikumbackendfrontendproject.Backend.controller;
 import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
 import technikumbackendfrontendproject.Backend.model.Product;
 import technikumbackendfrontendproject.Backend.model.DTO.ProductDTO;
+import technikumbackendfrontendproject.Backend.service.EntityNotFoundException;
 import technikumbackendfrontendproject.Backend.service.ProductService;
 
 @RestController
@@ -57,5 +59,15 @@ public class ProductController {
                 productDTO.getPrice(),
                 productDTO.getQuantity(),
                 productDTO.getType());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        try {
+            Product product = productService.getProduct(id);
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
--- Backend/src/main/java/technikumbackendfrontendproject/Backend/repository/ProductRepository.java
+++ Backend/src/main/java/technikumbackendfrontendproject/Backend/repository/ProductRepository.java
@@ -3,6 +3,8 @@ package technikumbackendfrontendproject.Backend.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import technikumbackendfrontendproject.Backend.model.Product;
