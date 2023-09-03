355d1b182b836cf4fd9c2def090dd786f672ca23
jesstify <jessica.fritsch@hotmail.com>
2023-09-03 10:04:45 +0200

    fixed display detailseite.

--- Frontend/detailseite_produkte.js
+++ Frontend/detailseite_produkte.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
         type: "GET",
         url: url,
         cors: true,
-        headers: {  },
+        headers: {},
         success: (product) => {
             console.log(product);
             container.append(displayProduct(product));
@@ -26,20 +26,33 @@ function getCurrentId() {
 
 function displayProduct(product) {
     const content = $(`
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <img class="card-img-top p-2" src="${product.imageUrl}" alt="Ein Bild von ${product.name}">
-                    <div class="card-body">
-                        <h1 class="card-title text-center">${product.name}</h4>
-                        <p class="card-text">${product.description}</p>
-                        <p>Type: ${product.type}</p>
-                        <p>Verfügbar: ${product.quantity}</p>
-                        <p>Preis: ${product.price}€</p>
-                    </div>
+    <div class="content" id="productContainer"></div>
+        <div class="container my-5">
+        
+            <div class="row">
+                <div class="col-lg-6 col-md-8 col-sm-12">                   
+                    <div  class="card">
+                        <img class="card-img-top p-2" src="${product.imageUrl}" alt="Ein Bild von ${product.name}">
+                            <div class="card-body">
+                                <h1 class="card-title text-center">${product.name}</h4>
+                                <p class="card-text">${product.description}</p>
+                                <p>Type: ${product.type}</p>
+                                <p>Verfügbar: ${product.quantity}</p>
+                                <p>Preis: ${product.price}€</p>
+                                <button type="button" class="btn btn-secondary">in Warenkorb</button>
+                            </div>
+                    </div>  
                 </div>
             </div>
-        </div>`       
+        </div>
+    </div>`
     );
     return content;
+
 };
+function changeStyle() {
+    var element = document.getElementById("productContainer");
+    element.style.color = "#7e8daf"
+    element.style.fontFamily = "sans-serif";
+}
+changeStyle();
