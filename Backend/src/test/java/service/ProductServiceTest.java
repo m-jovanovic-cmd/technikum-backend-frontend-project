776b7d186ae19aa7b9d64da2e3d511bd149d4a34
Mladen <mladenjovanovic95@hotmail.com>
2023-09-28 15:46:05 +0200

    additional configs and files for git actions

--- Backend/src/test/java/service/ProductServiceTest.java
+++ Backend/src/test/java/service/ProductServiceTest.java
@@ -1,2 +1,49 @@
-package service;public class ProductServiceTest {
+package service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import technikumbackendfrontendproject.Backend.BackendApplication;
+
+@SpringBootTest(classes = BackendApplication.class)
+@ActiveProfiles("test")
+public class ProductServiceTest {
+
 }
+
+/*
+import com.webshop.webshop.WebshopApplication;
+        import com.webshop.webshop.enums.ProductCategory;
+        import com.webshop.webshop.model.Product;
+        import com.webshop.webshop.repository.ProductRepository;
+        import com.webshop.webshop.service.ProductService;
+        import org.junit.jupiter.api.Assertions;
+        import org.junit.jupiter.api.BeforeEach;
+        import org.junit.jupiter.api.Test;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.boot.test.context.SpringBootTest;
+        import org.springframework.test.context.ActiveProfiles;
+
+        import static org.junit.Assert.assertEquals;
+
+
+@SpringBootTest(classes = WebshopApplication.class)
+@ActiveProfiles("test")
+class ProductServiceTest {
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @BeforeEach
+    void setup() {
+
+    }
+
+    @Test
+    void saveTest() {
+        assertEquals(1, 1);
+    }
+}
+*/
