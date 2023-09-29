e957793a323e66dd78be09692a44af001bd05a29
jesstify <jessica.fritsch@hotmail.com>
2023-09-29 13:50:00 +0200

    fixed saveUser function

--- Backend/src/main/java/technikumbackendfrontendproject/Backend/service/saveUser.java
+++
@@ -1,8 +0,0 @@
-package technikumbackendfrontendproject.Backend.service;
-
-import technikumbackendfrontendproject.Backend.model.DTO.UserDTO;
-import technikumbackendfrontendproject.Backend.model.User;
-
-public interface saveUser {
-    User saveUser(User user);
-}
